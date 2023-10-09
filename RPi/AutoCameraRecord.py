758eb4821d207b82abe8cbe143f500fa7828a49b
joshkuminski <119421535+joshkuminski@users.noreply.github.com>
2023-10-09 10:19:03 -0400

    Add files via upload

---
+++ RPi/AutoCameraRecord.py
@@ -0,0 +1,286 @@
+import mysql.connector
+import datetime
+#import subprocess
+import threading
+import time
+import queue
+import cv2
+
+
+def AddEntry():
+    Date='2023-05-05'
+    Time='0839'
+    Duration = 5
+
+    # Select All from Table
+    mycursor.execute("SELECT * FROM CameraSchedule")
+    for x in mycursor:
+        print(x)
+        
+    #Insert into Table
+    mycursor.execute("INSERT INTO CameraSchedule (Date, Time, Duration) VALUES (%s,%s,%s)",(Date, Time, Duration))
+    
+    db.commit()
+    
+
+def getNextEntry():
+    # Select all rows and order by date and time
+    mycursor.execute("SELECT * FROM CameraSchedule ORDER BY Date, Time")
+    rows = mycursor.fetchall()
+    
+    current_record = rows[0]
+
+    # Get the earliest date and time from the first row
+    earliest_time_str = str(rows[0][2])
+    earliest_hours = int(earliest_time_str[:2])
+    earliest_minutes = int(earliest_time_str[2:])
+    earliest_date = datetime.datetime.strptime(str(rows[0][1]), '%Y-%m-%d')
+    earliest_date_time = earliest_date.replace(hour=earliest_hours, minute=earliest_minutes)
+
+    # Get the current date and time
+    current_date_time = datetime.datetime.now()
+    
+    # Calculate the time difference
+    time_difference = earliest_date_time - current_date_time
+
+    # convert min to frames = time * 60 s/min * 15fps
+    duration = int(rows[0][3]) * 60 * 15
+
+    if int(time_difference.total_seconds()) > 0:
+        timer = threading.Timer(int(time_difference.total_seconds()), runCameraScript, args=[duration])
+        timer.start()
+        start_time = time.time()
+        print("timer started for " + str(time_difference) + " min.")
+        
+        # This will stay in a loop until timer expires
+        while timer.is_alive():
+            elapsed_time = time.time() - start_time
+            # check DB every 15min = 900 sec
+            if elapsed_time > 900:
+                
+                # reload the database
+                mycursor.execute("FLUSH TABLES;")
+                mycursor.execute("FLUSH PRIVILEGES;")
+                mycursor.execute("SET GLOBAL general_log = 'OFF';")
+                mycursor.execute("SET GLOBAL general_log = 'ON';")
+                
+                mycursor.execute("SELECT * FROM CameraSchedule ORDER BY Date, Time")
+                rows = mycursor.fetchall()
+                
+                if rows[0] != current_record:
+                    print("new receord found")
+                    return
+                
+                start_time = time.time()
+                print("No new record found")
+                
+            continue
+
+        # Call the Camera Record method
+        #runCameraScript(duration)
+
+        # time.sleep(60)
+
+        # Delte entry after sending to the Camera Record Script
+        mycursor.execute('DELETE FROM CameraSchedule WHERE CameraSchedule.ID=%s', (rows[0][0],))
+        
+    else:
+        mycursor.execute('DELETE FROM CameraSchedule WHERE CameraSchedule.ID=%s', (rows[0][0],))
+        print("entry deleted")
+        
+    db.commit()
+    
+
+def runCameraScript(record_dur):
+    #subprocess.Popen(["python", "/home/pi/scripts/Async_camera_test.py"])
+
+    # Create a VideoCapture object
+    cap = cv2.VideoCapture(0)
+
+    count = 0
+    # Check if capture was successful - try for five times
+    while not cap.isOpened() and count < 5:
+        print("Unable to open video stream. Trying again in 5 seconds...")
+        time.sleep(5)
+        cap = cv2.VideoCapture(0)
+        count += 1
+
+    if count == 5:
+        print("Unable to open video stream. Check your connection.")
+        exit()
+
+    # Set the video inpout to Full HD
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    cap.set(cv2.CAP_PROP_FPS, 15)  # set to 15 FPS
+
+    # Set the save image output to HD
+    screen_width = 1280 # width
+    screen_height = 720  # height
+    
+    # Get the frames per second (fps) and the frame size
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    date_time = datetime.datetime.now()
+    date_time = date_time.strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Define the codec and create a VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('/media/pi/New Volume/Output_{}.mp4'.format(date_time), fourcc, fps,
+                         (screen_width, screen_height))
+
+    # Queue to store the frames
+    frame_queue = queue.Queue()
+
+    # Flag to indicate if the writing thread should stop
+    stop_flag = False
+
+    # Function to write the frames to the video file
+    def write_frames():
+        while not stop_flag:
+            if not frame_queue.empty():
+                # Get frame from queue
+                frame = frame_queue.get()
+                # Resize the frame from Full HD (1920x1080) to HD (1280x720)
+                resized_frame = cv2.resize(frame, (screen_width, screen_height))
+                out.write(resized_frame)
+
+    # Start the writing thread
+    writing_thread = threading.Thread(target=write_frames)
+    writing_thread.start()
+
+    # Loop over the frames of the video
+    frame_num = 0
+    # duration = 600
+    while frame_num < record_dur:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        frame_num += 1
+
+        # If the frame was properly read, add it to the queue
+        if ret:
+            frame_queue.put(frame)
+            
+        if not ret:
+            break
+
+        # Break the loop if the 'q' key is pressed (troubleshooting)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Set the stop flag to True to indicate that the writing thread should stop
+    stop_flag = True
+
+    # Wait for the writing thread to finish
+    writing_thread.join()
+
+    # Release the VideoCapture and VideoWriter objects
+    cap.release()
+    out.release()
+
+    # Close all windows
+    cv2.destroyAllWindows()
+
+
+def delOldDates():
+    # execute a query to retrieve data from the database
+    mycursor.execute('SELECT * FROM CameraSchedule')
+
+    # fetch all the rows of data
+    rows = mycursor.fetchall()
+
+    # get the current date
+    current_date = datetime.datetime.now().date()
+    
+
+    # loop through the rows of data
+    for row in rows:
+        # extract the date from the row (assuming it's in the second column)
+        date = row[1]
+        #date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+
+        # check if the date is before the current date
+        if date < current_date:
+            # if so, delete the row from the database
+            mycursor.execute('DELETE FROM CameraSchedule WHERE CameraSchedule.ID=%s', (row[0],))
+
+    # commit the changes to the database
+    db.commit()
+
+    # close the database connection
+    
+def checkDuplicateEntries():
+    new_rows = []
+    # select all rows from the table
+    mycursor.execute('SELECT * FROM CameraSchedule')
+    rows =  mycursor.fetchall()
+
+    # create a set to keep track of unique rows
+    unique_rows = set()
+    for i in range(len(rows)):
+        new_rows.append(rows[i][1:])
+    print(new_rows)
+    # create a list to keep track of duplicate rows
+    duplicate_rows = []
+    
+    i = 0
+    # loop through the rows and check for duplicates
+    for row in new_rows:
+        # convert the row tuple to a string for comparison
+        row_str = str(row)
+        
+        # check if the row is already in the unique set
+        if row_str in unique_rows:
+            # add the row to the duplicate list
+            duplicate_rows.append(rows[i])
+        else:
+            # add the row to the unique set
+            unique_rows.add(row_str)
+            
+        i += 1
+
+    # check if there are any duplicates
+    if len(duplicate_rows) > 0:
+        # ask the user if they want to delete the duplicates
+        #response = input(f"There are {len(duplicate_rows)} duplicate rows. Do you want to delete them? (y/n) ")
+        #if response.lower() == 'y':
+            # delete the duplicate rows from the table
+        for row in duplicate_rows:
+             mycursor.execute('DELETE FROM CameraSchedule WHERE id=%s', (row[0],))
+        print("Duplicate rows deleted.")
+    #else:
+    #    print("Duplicate rows not deleted.")
+    else:
+        print("No duplicate rows found.")
+
+    # commit the changes and close the connection
+    db.commit()
+    
+
+if '__main__' == __name__:
+    record = True  # set to false to add to DB
+    
+    db = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                passwd="Radlich-1989",
+                database="CameraDB"
+            )
+
+    mycursor = db.cursor()
+            
+    if record:
+        while True:
+            
+            # Cleanup DB
+            delOldDates()
+            checkDuplicateEntries()
+        
+            # this will get the next time to record, start a timer and execute the record script
+            getNextEntry()
+             
+    else:
+        
+        AddEntry()
+
+    db.close()
---
+++ RPi/CameraGUI_5_customtk.py
@@ -0,0 +1,246 @@
+import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
+#import mysql.connector
+from tkinter import ttk, messagebox
+import subprocess
+import customtkinter as ctk
+from tkcalendar import Calendar
+
+
+# TODO: add check box to turn on/off the tilt servo
+
+class App(ctk.CTk):
+    def __init__(self, window, window_title, video_source=0):
+        super().__init__()
+        self.window = window
+
+        self.window.geometry("900x450")
+
+        # configure grid layout (4x4)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+        
+        # Create a variable to store the selected option
+        self.var = tk.StringVar()
+        self.var.set("Option 1")
+        # Create the radio buttons
+        radio_button1 = tk.Radiobutton(window, text="SD", variable=self.var, value="Option 1")
+        radio_button2 = tk.Radiobutton(window, text="HD", variable=self.var, value="Option 2")
+        radio_button3 = tk.Radiobutton(window, text="Full HD", variable=self.var, value="Option 3")
+
+        # Configure the command to be executed when a radio button is selected
+        radio_button1.config(command=self.show_selection)
+        radio_button2.config(command=self.show_selection)
+        radio_button3.config(command=self.show_selection)
+
+        # Pack the radio buttons into the window
+        radio_button1.grid(row=3, column=1, pady=20, padx=0)
+        radio_button2.grid(row=3, column=2, pady=20, padx=0)
+        radio_button3.grid(row=3, column=3, pady=20, padx=0)
+        #self.checkbox = ctk.CTkCheckBox(master=self.window)
+        #self.checkbox.grid(row=3, column=0, pady=20, padx=20, sticky="n")
+        #self.checkbox.configure(text="HD")
+        #self.checkbox.select()
+
+        ctk.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
+        ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+        # create database connection and cursor
+        #self.conn = mysql.connector.connect(
+        #            host="localhost",
+        #            user="root",
+        #            passwd="Radlich-1989",
+        #            database="CameraDB"
+        #            )
+        #self.c = self.conn.cursor()
+
+
+        # create treeview to display database
+        self.treeview = tk.ttk.Treeview(self.window, columns=("Date", "Time", "Duration"))
+        self.treeview.heading("#0", text="ID")
+        self.treeview.heading("Date", text="Date")
+        self.treeview.heading("Time", text="Time")
+        self.treeview.heading("Duration", text="Duration")
+        self.treeview.grid(row=0, column=0, padx=20, columnspan=12,  pady=(20, 10))
+
+        # add entries to treeview
+        self.add_entries()
+
+        # create button to add entry
+        self.add_button = tk.Button(self.window, text="Add Entry", command=self.add_entry_window)
+        self.add_button.grid(row=1, column=1, pady=(20, 10))
+        self.delete_button = tk.Button(self.window, text="Delete Entry", command=self.delete_entry)
+        self.delete_button.grid(row=1, column=2, pady=(20, 10))
+        self.edit_button = tk.Button(self.window, text="Edit Entry", command=self.edit_entry)
+        self.edit_button.grid(row=1, column=3, pady=(20, 10))
+        
+        self.edit_button.config(state='disabled')
+        
+        # create a button that opens a new window with the camera feed
+        self.btn2 = tk.Button(self.window, text="Open Camera Window", command=self.open_camera_window)
+        self.btn2.grid(row=2, column=0, padx=20, pady=(20, 10))
+
+        self.window.title(window_title)
+
+        # Bind the close event to the window
+        self.window.protocol("WM_DELETE_WINDOW", self.on_close)
+
+        # Set a flag to check if the video feed is running
+        self.is_running = False
+
+        # Start the tkinter mainloop
+        self.window.mainloop()
+    
+    def show_selection(self):
+        selected_option = self.var.get()
+        print("Selected option:", selected_option)
+        # TODO put a new column in DB for the desired resolution
+        
+    def open_camera_window(self):
+        # create a new window with the camera feed
+        self.camera_window = tk.Toplevel(self.window)
+        self.camera_window.title("Camera Window")
+
+        # Bind the close event to the window
+        self.camera_window.protocol("WM_DELETE_WINDOW", self.cam_close)
+
+        # create a label to display the camera feed
+        label = tk.Label(self.camera_window)
+        label.grid(row=0, column=0, padx=20, pady=(20, 10))
+
+        # initialize the camera capture
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(3, 1920)
+        self.cap.set(4, 1080)
+        self.cap.set(cv2.CAP_PROP_FPS, 15)
+        screen_width = 750  # Example screen width
+        screen_height = 360  # Example screen height
+        
+
+        #tilt = self.checkbox.get()
+
+        # update the label with each new frame from the camera
+        def update_frame():
+            ret, frame = self.cap.read()
+            resized_frame = cv2.resize(frame, (screen_width, screen_height))
+            
+            if ret:
+                # convert the frame to PIL format and display it on the label
+                img = Image.fromarray(cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB))
+                imgtk = ImageTk.PhotoImage(image=img)
+                label.imgtk = imgtk
+                label.configure(image=imgtk)
+            label.after(10, update_frame)
+
+        # start updating the label with new frames
+        update_frame()
+
+    def add_entries(self):
+        # clear existing entries from treeview
+        for i in self.treeview.get_children():
+            self.treeview.delete(i)
+        
+        #self.c.execute("SELECT * FROM CameraSchedule")
+        #rows = self.c.fetchall()
+        
+        # query database and add entries to treeview
+        #for row in rows:
+        #    self.treeview.insert('', 'end', text=row[0], values=(row[1], row[2], row[3]))
+
+    def add_entry(self,date, time, duration):
+        # insert new entry into database
+        self.c.execute("INSERT INTO CameraSchedule (Date, Time, Duration) VALUES (%s, %s, %s)", (date, time, duration))
+        self.conn.commit()
+
+        # update treeview
+        self.add_entries()
+
+    def add_entry_window(self):
+        # create new window for adding an entry
+        self.new_window = tk.Toplevel(self.window)
+        self.new_window.title("Add Entry")
+        
+        def select_date():
+            selected_date = cal.get_date()
+            date_entry.delete(0, tk.END)
+            date_entry.insert(0, selected_date)
+            
+
+        root = tk.Tk()
+        root.title("Date Picker Example")
+
+        cal = Calendar(root, selectmode="day", date_pattern="yyyy-mm-dd")
+        cal.pack(pady=20)
+
+        select_button = tk.Button(root, text="Select Date", command=select_date)
+        select_button.pack(pady=10)
+        
+        # create entry fields for self.new_window        
+        date_label = tk.Label(self.new_window, text="Date:")
+        date_label.grid(row=0, column=0)
+        date_entry = tk.Entry(self.new_window)
+        date_entry.grid(row=0, column=1)
+        
+
+        time_label = tk.Label(self.new_window, text="Time:")
+        time_label.grid(row=1, column=0)
+        # Generate time values in 15-minute increments
+        times = []
+        for hour in range(5, 21):
+            for minute in range(0, 60, 15):
+                time = f"{hour:02d}{minute:02d}"
+                times.append(time)
+
+        self.time_entry = ttk.Combobox(self.new_window, textvariable=tk.StringVar())
+        self.time_entry['values'] = times
+        self.time_entry.grid(row=1, column=1)
+        
+        duration_label = tk.Label(self.new_window, text="Duration:")
+        duration_label.grid(row=2, column=0)
+        duration_entry = ttk.Combobox(self.new_window, textvariable=tk.StringVar())
+        duration_entry['values'] = list(range(15, 181, 15))
+        duration_entry.grid(row=2, column=1)
+        
+        
+        # create button to add entry to database
+        add_button = tk.Button(self.new_window, text="Add",
+                               command=lambda: self.add_entry(date_entry.get(), self.time_entry.get(), duration_entry.get()))
+        add_button.grid(row=3, column=1)
+        
+    def delete_entry(self):
+        selected_item = self.treeview.focus()  # Get the currently selected item
+        if selected_item:  # If an item is selected
+            value = self.treeview.item(selected_item)['text']  # Retrieve the value of the first column
+            print(value)  # Do something with the value
+            self.treeview.delete(selected_item)  # Delete the selected item
+        # Delete entry from database
+        self.c.execute("DELETE FROM CameraSchedule WHERE ID = (%s)", (value,))
+        self.conn.commit()
+        
+    def edit_entry(self):
+        selected_item = self.treeview.focus()  # Get the currently selected item
+        if selected_item:  # If an item is selected
+            values = self.treeview.item(selected_item)['values']  # Retrieve the value of the first column
+            #self.time_entry.insert(0, values[1])
+            self.time_entry.current(values.index(default_value))
+            _id = self.treeview.item(selected_item)['text']
+            print(values)  # Do something with the value
+            #self.treeview.delete(selected_item)  # Delete the selected item
+            add_entry_window()
+    
+    def on_close(self):
+        self.window.destroy()
+        exit()
+
+    def cam_close(self):
+        self.cap.release()
+        self.camera_window.destroy()
+
+
+# Create the App object with the window title and video source
+if __name__ == "__main__":
+    app = App(ctk.CTk(), "USB Camera Feed")
+    app.mainloop()
+
